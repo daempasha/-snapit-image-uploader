c0366368dcafc06da5ff0ecd891b3bf376e39e5c
Daem Pasha <daempasha@gmail.com>
2022-12-04 22:21:39 +0000

    Added SSL port

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 443
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.listen(port, () => {
-    console.log(`App listening onport ${port}`)
+    console.log(`App listening on port ${port}`)
 })
