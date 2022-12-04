228b1f31cd5d342454435c52910812215f32eef2
Daem Pasha <daempasha@gmail.com>
2022-12-04 23:06:36 +0000

    Added dotenv config to backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const dotenv = require("dotenv")
+
+dotenv.config()
+
 const app = express()
-const port = 443
+const port = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
