318659d7927c5a5250893ae14dce28a8ca243c7a
Daem Pasha <daempasha@gmail.com>
2022-12-13 13:15:50 +0000

    App now uses state from zuustand to switch comp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,26 +2,39 @@ import React from 'react';
 import Card from 'components/Card/card.component';
 import CardContainer from 'components/CardContainer/cardcontainer.component';
 import Dropzone from 'components/Dropzone/dropzone.component';
+import { useStore } from 'store';
+import Uploading from 'components/Uploading/uploading.component';
+import Uploaded from "components/Uploaded/uploaded.component"
+import { Toaster } from "react-hot-toast";
 function App() {
+  const state = useStore(store => store.state);
   return (
-    <div className="bg-gray-200 flex flex-col min-h-screen h-screen font-sans">
+    <>
+      <Toaster />
+      <div className="bg-gray-200 flex flex-col min-h-screen h-screen font-sans">
 
-      <main className='flex-1 w-full'>
+        <main className='flex-1 w-full'>
 
-        <CardContainer>
-          <Card>
-            <h1 className="text-center font-medium text-xl">Upload your image</h1>
-            <p className="text-center text-xs my-3 text-gray-500">File should be .jpeg, .png</p>
-            <Dropzone />
-          </Card>
+          <CardContainer>
+            {state === 'upload' &&
+              <Card>
+                <h1 className="text-center font-medium text-xl">Upload your image</h1>
+                <p className="text-center text-xs my-3 text-gray-500">File should be .jpeg, .png</p>
+                <Dropzone />
+              </Card>
+            }
 
-        </CardContainer>
+            {state === "uploading" && <Uploading />}
 
-      </main>
-      <footer className='text-center text-gray-500 py-5'>
-        created by <a className='underline font-bold hover:text-gray-600 transition-all' rel="noreferrer" target="_blank" href="https://github.com/daempasha">daempasha</a>
-      </footer>
-    </div>
+            {state === "uploaded" && <Uploaded />}
+          </CardContainer>
+
+        </main>
+        <footer className='text-center text-gray-500 py-5'>
+          created by <a className='underline font-bold hover:text-gray-600 transition-all' rel="noreferrer" target="_blank" href="https://github.com/daempasha">daempasha</a>
+        </footer>
+      </div>
+    </>
   );
 }
 
