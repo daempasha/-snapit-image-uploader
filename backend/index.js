28d776c41b0d3517fbb9cf273429609fffc8d209
Daem Pasha <daempasha@gmail.com>
2022-12-13 13:12:58 +0000

    Backend now uploads file to cloudinary and returns url

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,56 @@
 const express = require('express')
+const cors = require('cors')
+const multer = require("multer");
+const fs = require("fs")
 const dotenv = require("dotenv")
+const cloudinary = require("cloudinary");
+const app = express()
+const upload = multer({ dest: "uploads/" });
 
 dotenv.config()
 
-const app = express()
-const port = process.env.PORT || 8000
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+app.use(cors())
+app.use(express.json())
+const port = 8000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+
+async function uploadToCloudinary(localPath) {
+    var mainFolderName = "main";
+    var filePathOnCloudinary =
+        mainFolderName + "/" + localPath;
+
+    return cloudinary.uploader
+        .upload(localPath, { public_id: filePathOnCloudinary })
+
+}
+
+app.post("/api/upload-file", upload.single("image"), async (req, res) => {
+    const localPath = req.file.path
+    const response = await uploadToCloudinary(localPath)
+        .then((result) => {
+            fs.unlinkSync(localPath);
+
+            res.status(201).send({
+                message: "Successfully uploaded image!",
+                url: result.url,
+            });
+        })
+        .catch((error) => {
+            fs.unlinkSync(localPath);
+            res.status(400).send({ message: "Failed to upload image!", url: undefined });
+        });
+
+    res.send(response)
 })
 
 app.listen(port, () => {
